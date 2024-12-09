6a525cad34ac6acfa7725c8a568f274d5f9ae41e
DaNKAadnka <danka2202@yandex.ru>
2024-12-09 22:19:45 +0300

    deploy changes v1

--- Postgres/tables.sql
+++ Postgres/tables.sql
@@ -1,36 +1,36 @@
-create table if not exists user (
+create table if not exists users (
     user_id serial primary key,
     username varchar(255) not null unique,
     email varchar(255) not null unique,
     password_hash varchar(255) not null
 );
 
-CREATE TABLE if not exists token (
+create table if not exists tokens (
     token_id UUID PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(user_id) ON DELETE CASCADE,
     issued_at TIMESTAMP NOT NULL,
     expired_at TIMESTAMP NOT NULL
 );
 
-create table if not exists group_user (
-    group_user_id serial primary key,
-    group_id int REFERENCES groups(id) ON DELETE CASCADE,
-    user_id int REFERENCES users(id) ON DELETE CASCADE
-);
-
-create table if not exists group (
+create table if not exists groups (
     group_id serial primary key,
     name_group varchar(255) not null unique
 );
 
-create table if not exists word (
+create table if not exists words (
     word_id serial primary key,
     rus_word varchar(255) not null,
     translation varchar(255) not null
 );
 
-create table if not exists group_word (
+create table if not exists groups_users (
+    group_user_id serial primary key,
+    group_id int REFERENCES groups(group_id) ON DELETE CASCADE,
+    user_id int REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+create table if not exists groups_words (
     group_word_id serial primary key,
-    group_id int REFERENCES groups(id) ON DELETE CASCADE,
-    word_id int REFERENCES words(id) ON DELETE CASCADE
+    group_id int REFERENCES groups(group_id) ON DELETE CASCADE,
+    word_id int REFERENCES words(word_id) ON DELETE CASCADE
 );
